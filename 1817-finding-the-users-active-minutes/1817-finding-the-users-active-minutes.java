77604f9b9d94c452563dcc55ffc6302a119b43d5
LOGITH2003 <110965723+LOGITH2003@users.noreply.github.com>
2024-05-06 10:41:54 +0530

    Update the comment in 1817-finding-the-users-active-minutes.java

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -1,8 +1,12 @@
 class Solution {
     public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        
         int [] arr = new int[k];
 
+        // create the hashmap to sort the user entry time and using hashset due to don't add the duplicate entry
         HashMap<Integer, HashSet<Integer>> list = new HashMap<>();
+
+        // iterate the array to store in hashmap
         for (int[] log : logs) {
             HashSet<Integer> ls;
             if (list.containsKey(log[0])) {
@@ -14,11 +18,12 @@ class Solution {
             list.put(log[0], ls);
         }
 
+        // iterate the hashmap value and add the count of the value 
         for(var val : list.entrySet()){
             arr[val.getValue().size()-1]++;
         }
-//        System.out.println(list);
 
+        // return the array
         return arr;
     }
 }
