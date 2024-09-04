a81ee3b8821601fb8d83344b017edad5b38548a5
LOGITH J <110965723+LOGITH2003@users.noreply.github.com>
2024-09-04 10:36:59 +0530

    Time: 679 ms (23.50%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select employee_id , case when employee_id%2=1 and substring(name,1,1) != 'M' then salary else 0 end as bonus from Employees order by employee_id ;
